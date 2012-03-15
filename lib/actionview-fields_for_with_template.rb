48e6fb282a470559287282aee625e7383bcc0079
Ryan Glover <ersatzryan@gmail.com>
2012-03-15 12:59:56 -0400

    Removes setting child_index option to 0

--- lib/actionview-fields_for_with_template.rb
+++ lib/actionview-fields_for_with_template.rb
@@ -3,10 +3,6 @@ module ActionView
   module Helpers
     class FormBuilder
       def fields_for_with_template(record_or_name_or_array, *args, &block)
-        options = args.extract_options!
-        options[:child_index] = 0
-        args << options
-
         result = fields_for record_or_name_or_array, *args, &block
         result += fields_for record_or_name_or_array, object.send(record_or_name_or_array).new, :child_index => "NEW_RECORD" do |f|
           @template.concat %|<div class="template" style="display:none">|.html_safe
